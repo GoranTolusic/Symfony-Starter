a35c406b2cffefb861387f4ccbf11522d49ae1fa
GoranTolusic <goran.tolusic@gmail.com>
2025-08-30 16:34:43 +0200

    script for quick creating db

--- create_db.php
+++ create_db.php
@@ -1,20 +1,25 @@
 <?php
-// load .env variables ili postavi ručno
-$DB_USER = getenv('DB_USER') ?: 'root';
-$DB_PASS = getenv('DB_PASS') ?: '1234';
-$DB_HOST = getenv('DB_HOST') ?: '172.17.0.2';
-$DB_PORT = getenv('DB_PORT') ?: '3306';
-$DB_NAME = getenv('DB_NAME') ?: 'symfony_starter';
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$DB_USER = $_ENV['DB_USER'] ?: 'root';
+$DB_PASS = $_ENV['DB_PASS'] ?: '1234';
+$DB_HOST = $_ENV['DB_HOST'] ?: '172.17.0.2';
+$DB_PORT = $_ENV['DB_PORT'] ?: '3306';
+$DB_NAME = $_ENV['DB_NAME'] ?: 'symfony_starter';
 
 try {
-    // konekcija bez baze, da možemo provjeriti/kreirati
+    //Initialize connection to db
     $pdo = new PDO("mysql:host=$DB_HOST;port=$DB_PORT;charset=utf8mb4", $DB_USER, $DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // provjeri postoji li baza
+    //Check if database with same name already exists
     $stmt = $pdo->query("SHOW DATABASES LIKE '$DB_NAME'");
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($result);
+
+    //If not, we are creating one
     if ($stmt->rowCount() === 0) {
         echo "Baza '$DB_NAME' ne postoji. Kreiram...\n";
         $pdo->exec("CREATE DATABASE `$DB_NAME` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci");
