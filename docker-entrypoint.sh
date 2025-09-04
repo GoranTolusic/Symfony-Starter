07576788734357ea7310f483c4e0b30245c53907
GoranTolusic <goran.tolusic@gmail.com>
2025-09-04 12:31:15 +0200

    modify entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 set -e
 
-echo "Čekam DNS za MySQL ($DB_HOST)..."
+echo "Waiting DNS for MySQL ($DB_HOST)..."
 
-# čekaj dok hostname 'db' može da se resolve-a
+# Wait until host db is resolved
 until getent hosts "$DB_HOST" >/dev/null 2>&1; do
   sleep 1
 done
 
-echo "Čekam MySQL na $DB_HOST:$DB_PORT..."
+echo "Waiting MySQL on $DB_HOST:$DB_PORT..."
 
-# čekaj dok MySQL stvarno ne bude spreman
+# Wait for MySQL to be ready
 until php -r "new PDO('mysql:host=${DB_HOST};port=${DB_PORT}','${DB_USER}','${DB_PASS}');" >/dev/null 2>&1; do
   sleep 1
 done
 
-echo "MySQL je spreman!"
+echo "MySQL is ready!"
 
-# Pokreni create_db.php ako treba
+#Instalation process: creating database and migrating tables
 if [ -f create_db.php ]; then
-    php create_db.php
+    composer create-db
 fi
+composer migrate || true
 
-# Pokreni Doctrine migracije
-php bin/console doctrine:migrations:migrate --no-interaction || true
+#Clear cache
+composer cache-clear
 
-echo "Pokrećem PHP-FPM..."
-# exec starta PHP-FPM i zamjenjuje skriptu
+#Executing php-fpm process
+echo "Executing PHP-FPM..."
 exec php-fpm
