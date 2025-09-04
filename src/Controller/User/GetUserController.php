54c2ddf64d9149fd07fa498e6a55b2ee8061418d
GoranTolusic <goran.tolusic@gmail.com>
2025-09-04 11:33:34 +0200

    removed unnecesary services from controllers

--- src/Controller/User/GetUserController.php
+++ src/Controller/User/GetUserController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Traits\MiddlewareTrait;
 
 class GetUserController extends AbstractController
@@ -15,15 +14,13 @@ class GetUserController extends AbstractController
     use MiddlewareTrait;
 
     private GetUserService $userService;
-    private ValidatorInterface $validator;
 
-    public function __construct(GetUserService $userService, ValidatorInterface $validator)
+    public function __construct(GetUserService $userService)
     {
         $this->userService = $userService;
-        $this->validator = $validator;
     }
 
-    #[Route('/user/{id}', name: 'user_myProfile', methods: ['GET'])]
+    #[Route('/user/{id}', name: 'user_getOne', methods: ['GET'])]
     public function getOneUser(Request $request, int $id): JsonResponse
     {
         $this->authenticateUser($request);
--- src/Controller/User/UpdateUserController.php
+++ src/Controller/User/UpdateUserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\User;
 
-use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
