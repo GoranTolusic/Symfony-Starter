c5bc551ef57287c0c30f22e5dad9c9a8230ce997
GoranTolusic <goran.tolusic@gmail.com>
2025-09-05 21:21:59 +0200

    fix run.sh script

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -e
 
+# Clear old containers
 docker container prune -f
 
+# Build containers
 docker-compose build
 
-docker-compose up
+# Run compose in detached mode
+docker-compose up -d
 
+# Run tests in container
+if ! docker-compose run --rm app-test composer start-test; then
+    echo "Tests failed! Stopping all containers..."
+    docker-compose down
+    exit 1
+fi
 
+echo "Tests passed! Showing app logs..."
+
+docker logs -f app
