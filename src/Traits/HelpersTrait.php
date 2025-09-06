1d2f36e9befe30424b388d3bf662f3e78fb399e7
GoranTolusic <goran.tolusic@gmail.com>
2025-09-06 19:00:23 +0200

    random string generator simplified

--- src/Traits/HelpersTrait.php
+++ src/Traits/HelpersTrait.php
@@ -4,29 +4,22 @@ namespace App\Traits;
 
 trait HelpersTrait
 {
-    function generateRandomString($length = 10)
-    {
+    function generateRandomString($length = 10) {
         $length = max(3, min(50, $length));
-        $lower  = 'abcdefghijklmnopqrstuvwxyzšđčćž';
-        $upper  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZŠĐČĆŽ';
+    
+        $lower = 'abcdefghijklmnopqrstuvwxyz';
+        $upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $digits = '0123456789';
-        $all    = $lower . $upper . $digits;
-
-        $pick = function ($string) {
-            $len = mb_strlen($string, 'UTF-8');
-            return mb_substr($string, random_int(0, $len - 1), 1, 'UTF-8');
-        };
-
-        $result = [];
-        $result[] = $pick($lower);
-        $result[] = $pick($upper);
-        $result[] = $pick($digits);
-
+        $all = $lower . $upper . $digits;
+    
+        $result = $lower[random_int(0, strlen($lower) - 1)];
+        $result .= $upper[random_int(0, strlen($upper) - 1)];
+        $result .= $digits[random_int(0, strlen($digits) - 1)];
+    
         for ($i = 3; $i < $length; $i++) {
-            $result[] = $pick($all);
+            $result .= $all[random_int(0, strlen($all) - 1)];
         }
-
-        shuffle($result);
-        return implode('', $result);
+    
+        return str_shuffle($result);
     }
 }
