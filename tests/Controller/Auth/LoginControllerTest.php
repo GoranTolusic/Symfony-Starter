6671621e0bcbc434f403431a58895055908f4e8d
GoranTolusic <goran.tolusic@gmail.com>
2025-09-06 14:54:19 +0200

    login test controller

---
+++ tests/Controller/Auth/LoginControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Tests\Controller\Auth;
+
+use App\Tests\HttpTestCase;
+use App\Tests\EnvConfig;
+
+/**
+ * @group auth
+ */
+class LoginControllerTest extends HttpTestCase
+{
+    private EnvConfig $env;
+
+    //HttpTestCase has final __constructor so we cant override it, but we can use setUp which is called before every test by defaul
+    //We are using it to setup singleton env config so we can have quick access to some useful variables
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->env = EnvConfig::getInstance();
+    }
+
+    public function testLoginWithValidPayload(): void
+    {
+        $payload = [
+            "email" => $this->env->get('TEST_EMAIL'),
+            "password" => $this->env->get('TEST_PASSWORD'),
+        ];
+
+        $response = $this->postJson('/auth/login', $payload);
+        $this->assertContains(
+            $response['status'],
+            [200],
+            "Expected status 200 (success), got {$response['status']}"
+        );
+
+        //TODO: set token in test env
+    }
+
+    public function testLoginWithInvalidPayload(): void
+    {
+        $payload = [
+            'email' => 'not-an-email',
+            'password' => ''
+        ];
+
+        $response = $this->postJson('/auth/login', $payload);
+        $this->assertContains(
+            $response['status'],
+            [400],
+            "Expected status 400 (Bad Request), got {$response['status']}"
+        );
+    }
+
+    public function testLoginWithInvalidCredentials(): void
+    {
+        $payload = [
+            'email' => $this->env->get('TEST_EMAIL'),
+            'password' => 'invalid123Password'
+        ];
+
+        $response = $this->postJson('/auth/login', $payload);
+        $this->assertContains(
+            $response['status'],
+            [401],
+            "Expected status 401 (Unauthorized), got {$response['status']}"
+        );
+    }
+}
